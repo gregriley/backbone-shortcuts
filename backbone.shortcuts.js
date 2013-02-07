21fc66eb07c1a0f44a4a6d4c817fabc489aa0691
Vitaly <orloff.v@gmail.com>
2013-02-07 11:50:27 +0400

    fix many events for one element

--- backbone.shortcuts.js
+++ backbone.shortcuts.js
@@ -1,89 +1,94 @@
-(function() {
-  var Shortcuts;
+(function () {
+    var Shortcuts;
 
-  Shortcuts = function(options) {
-    this.cid = _.uniqueId("backbone.shortcuts");
-    this.initialize.apply(this, arguments);
-    return this.delegateShortcuts();
-  };
+    Shortcuts = function (options) {
+        this.cid = _.uniqueId("backbone.shortcuts");
+        this.initialize.apply(this, arguments);
+        return this.delegateShortcuts();
+    };
 
-  _.extend(Shortcuts.prototype, Backbone.Events, {
-    initialize: function() {},
-    delegateShortcuts: function() {
-      var callback, match, method, scope, shortcut, shortcutKey, _ref, _results;
-      if (!this.shortcuts) return;
-      _ref = this.shortcuts;
-      _results = [];
-      for (shortcut in _ref) {
-        callback = _ref[shortcut];
-        if (!_.isFunction(callback)){
-              method = this[callback];
-            if (!method) throw new Error("Method " + callback + " does not exist");
-        }
-        else {
-            method = callback;
-        }
-        match = shortcut.match(/^(.*?)\s*(<.*>)?$/);
-        shortcutKey = match[1];
-        var element = typeof match[2] === 'undefined' ? null : match[2].replace('>', '').replace('<', '')
-        scope = 'all';
+    _.extend(Shortcuts.prototype, Backbone.Events, {
+        initialize:function () {
+        },
+        delegateShortcuts:function () {
+            var callback, match, method, scope, shortcut, shortcutKey, _ref, _results;
+            if (!this.shortcuts) return;
+            _ref = this.shortcuts;
+            _results = [];
+            for (shortcut in _ref) {
+                callback = _ref[shortcut];
+                if (!_.isFunction(callback)) {
+                    method = this[callback];
+                    if (!method) throw new Error("Method " + callback + " does not exist");
+                }
+                else {
+                    method = callback;
+                }
+                match = shortcut.match(/^(.*?)\s*(<.*>)?$/);
+                shortcutKey = match[1];
+                var element = typeof match[2] === 'undefined' ? null : match[2].replace('>', '').replace('<', '');
+                scope = 'all';
 
-        method = _.bind(method, this);
-        if (element) {
-            _results.push(
-                key(
-                    shortcutKey,
-                    scope,
-                    method,
-                    function(e) {
-                        if (element === 'all') {
-                            return true;
-                        }
+                method = _.bind(method, this);
+                if (element) {
+                    _results.push(
+                        key(
+                            shortcutKey,
+                            scope,
+                            method,
+                            element,
+                            function (e, localElement) {
 
-                        var uniqId = _.uniqueId('shortcut');
-                        $(e.target).data('shortcutId', uniqId);
+                                if (localElement === 'all') {
+                                    return true;
+                                }
 
-                        return $(element, this.$el).data('shortcutId') == uniqId;
-                    }
-                )
-            );
-        } else {
-            _results.push(
-                key(
-                    shortcutKey,
-                    scope,
-                    method,
-                    function(e) {
-                        var tagName = (e.target || e.srcElement).tagName;
+                                if (_.isUndefined($(localElement, this.$el).data('shortcutId'))) {
+                                    $(localElement, this.$el).data('shortcutId', _.uniqueId('shortcut'))
+                                }
 
-                        return !(tagName == 'INPUT' || tagName == 'SELECT' || tagName == 'TEXTAREA');
-                    }
-                )
-            );
-        }
-      }
-      return _results;
-    },
-    undelegateShortcuts: function() {
-        if (!this.shortcuts) return;
-        var _ref, shortcut, match, shortcutKey, scope;
-        _ref = this.shortcuts;
-        for (shortcut in _ref) {
-            match = shortcut.match(/^(.*?)\s*(<.*>)?$/);
-            shortcutKey = match[1];
-            scope = 'all';
+                                return $(e.target).data('shortcutId') === $(localElement, this.$el).data('shortcutId');
+                            }
+                        )
+                    );
+                } else {
+                    _results.push(
+                        key(
+                            shortcutKey,
+                            scope,
+                            method,
+                            element,
+                            function (e, localElement) {
+                                var tagName = (e.target || e.srcElement).tagName;
 
-            shortcutKey = shortcutKey.replace(/\s/g,'');
-            var keys = shortcutKey.split(',');
-            for (i = 0; i < keys.length; i++) {
-                key.unbind(keys[i], scope);
+                                return !(tagName == 'INPUT' || tagName == 'SELECT' || tagName == 'TEXTAREA');
+                            }
+                        )
+                    );
+                }
+            }
+            return _results;
+        },
+        undelegateShortcuts:function () {
+            if (!this.shortcuts) return;
+            var _ref, shortcut, match, shortcutKey, scope;
+            _ref = this.shortcuts;
+            for (shortcut in _ref) {
+                match = shortcut.match(/^(.*?)\s*(<.*>)?$/);
+                shortcutKey = match[1];
+                scope = 'all';
+
+                shortcutKey = shortcutKey.replace(/\s/g, '');
+                var keys = shortcutKey.split(',');
+                for (i = 0; i < keys.length; i++) {
+                    key.unbind(keys[i], scope);
+                }
             }
         }
-    }
-  });
+    });
 
-  Backbone.Shortcuts = Shortcuts;
+    Backbone.Shortcuts = Shortcuts;
 
-  Backbone.Shortcuts.extend = Backbone.View.extend;
+    Backbone.Shortcuts.extend = Backbone.View.extend;
 
 }).call(this);
